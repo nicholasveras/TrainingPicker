079d1dee09731aa9f45643ea2b697daad9a1ed6f
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2023-12-18 13:02:43 -0300

    TrainingPicker

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 export default function App() {
+  
+  const [carroSelecionado, setCarroSelecionado] = useState (0);
+  const [carros, setCarros] = useState([
+    {key: 1, nome: 'Golf Highline 1.4', valor: 80.000},
+    {key: 2, nome: 'Jetta', valor: 72.000},
+    {key: 3, nome: 'Saveiro', valor: 58.000},
+    {key: 4, nome: 'Nivus', valor: 115.000},
+    {key: 5, nome: 'Gol G6', valor: 52.000},
+  ]);
+
+  let carrosItem = carros.map((value, key) => {
+    return <Picker.Item key={key} value={key} label= {value.nome}/>
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Picker
+      selectedValue={carroSelecionado}
+      onValueChange={ (itemValue, itemIndex) => setCarroSelecionado(itemValue) }
+      >
+        {carrosItem}
+      </Picker>
+
+<Text style={styles.carros}>Carro: {carros[carroSelecionado.nome]}</Text>
+<Text style={styles.carros}>Carro: {carros[carroSelecionado.valor.toFixed(3)]}</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +39,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 35,
   },
+  carros: {
+    marginTop: 15,
+    fontSize: 25,
+    
+  }
 });
